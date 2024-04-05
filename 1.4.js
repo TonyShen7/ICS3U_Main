e988d350a59bbebd420c86f580173462923b244c
TonyShen7 <Tony.shen@stu.ocsb.ca>
2024-04-05 14:18:05 -0400

    4

--- 1.4.js
+++ 1.4.js
@@ -1,7 +1,8 @@
 const prompt = require('prompt-sync')();
 let year = prompt("When were you born?");
 console.log("You were born on: "+year);
-let old = year-2024;
+let old = 2024-year;
 console.log("you are likely "+old);
 let name = prompt("What is your name");
 console.log("Hi "+name+"! You were born on "+year+" and you are "+old+" years old!");
+console.log("Your name is "+name.length+" long");
